ee3394cdc1400f5017d758ae417d6f5134995aca
Lisa <llvvss@icloud.com>
2021-11-17 16:59:15 -0700

    minor edits and adding author name

--- src/com/example/findstring/FindString.java
+++ src/com/example/findstring/FindString.java
@@ -5,10 +5,17 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;       // Import the Scanner class
 
+/***
+ * Date: Wednesday, November 17, 2021
+ * Author: Lisa Saffel
+ * Purpose: Input a string and pathname from the user and print out all files
+ *          in that directory, including the path,that contain that string.
+ */
+
 public class FindString {
 
     public static void main(String[] args) throws IOException {
-        // input the string to search for and the path
+        // From the user, input the string to search for and the path.
 
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
 
@@ -27,9 +34,9 @@ public class FindString {
 
         System.out.println("The files (including paths) that contain the search string are the following,");
         System.out.println("or if there are no files containing the search string, then this will be blank:");
+        System.out.println("");
 
         findTheString(inputSearchString, inputSearchPath);
-
     }
 
     private static void findTheString(String searchString, String searchPath) throws IOException {
@@ -40,35 +47,33 @@ public class FindString {
         // returns pathnames for files and directory
         File[] paths = f.listFiles();
 
-
-
         // for each pathname in pathname array
         for(File path:paths) {
-            // For any files in this directory, search for the string and if found, print that file name
-            // For any directories in this directory, go into that directory and do this search again
+            // For any files in this directory, search for the string and if found, print that file name.
+            // For any directories in this directory, go into that directory and do this search again.
 
             if (path.isDirectory()) {
-                // what we're pointing at is a directory, so do a recursive search on the directory
+                // What we're pointing at is a directory, so do a recursive search on the directory.
 
                 findTheString(searchString, path.toString());
 
             } else {
-                // if the string is in the file then print the name of the file
+                // What we are pointing at is a file.
+                // Check if the string is in the file, and if it is, then print the name of the file.
 
                 BufferedReader br = new BufferedReader(new FileReader(path));
                 String line;
                 stringFound = false;    // initialize
                 while ((line = br.readLine()) != null) {
-                    // check if the string searchString is in the line line in the file
-                    // if it is, print the path and file name
-                    // if the string is not in that line of the file, then do nothing
+                    // Check if the string searchString is in the line "line" in the file.
+                    // If it is, print the path and file name.
+                    // If the string is not in that line of the file, then do nothing.
 
                     if (line.contains(searchString)) {
                         stringFound = true;
                     }
                 }
                 if (stringFound) {
-//                    System.out.println("%% woohoo woohoo %% The file DOES contain that string and the file is called:");
                     System.out.println(path);
                 }
             }
